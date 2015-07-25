27c19db1e208b5fcf1a25e162763b39f3167b31b
ddyeakley <ddyeakley@gmail.com>
2015-07-25 09:43:25 -0400

    Initial check in

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='networkmonitor',
+    version='0.1',
+    license='MIT',
+    author='Dan Yeakley',
+    author_email='ddyeakley@gmail.com',
+    url='https://github.com/ddyeakley/networkmonitor',
+    long_description="README.txt",
+    packages=['networkmonitor'],
+    include_package_data=false,
+    description="Simple Python Network Monitor tool",
+)
